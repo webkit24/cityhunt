74097b6bba5b9085786f0b69c1a437d88296f752
webkit24 <sudhanshu@webkit24.com>
2017-03-18 02:10:44 +0530

    push all

---
+++ header.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Listable
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-72520704-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<meta name="google-site-verification" content="-MDsYT0qr9bk8JdbicK58o3EzC2bB6OUkvzlPgk-1is" />
+
+
+<script src="https://www.gstatic.com/firebasejs/3.6.7/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDK_s_2fT0TlFL4CdxXCGa2fxgADX2BhKs",
+    authDomain: "cityhunt-156410.firebaseapp.com",
+    databaseURL: "https://cityhunt-156410.firebaseio.com",
+    storageBucket: "cityhunt-156410.appspot.com",
+    messagingSenderId: "832270378707"
+  };
+  firebase.initializeApp(config);
+</script>
+</head>
+<body <?php body_class(); ?> data-mapbox-token="<?php echo listable_get_option('mapbox_token', ''); ?>" data-mapbox-style="<?php echo listable_get_option('mapbox_style', ''); ?>">
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'listable' ); ?></a>
+
+	<header id="masthead" class="site-header  <?php if( is_page_template( 'page-templates/front_page.php' ) && (listable_get_option( 'header_transparent', true ) == true) ) echo 'header--transparent'; ?>" role="banner">
+		<?php listable_display_logo(); ?>
+
+		<?php get_template_part( 'template-parts/header-fields' ); ?>
+
+
+		<?php
+		// Output the navigation and mobile nav button only if there is a nav
+		if ( has_nav_menu( 'primary' ) || has_nav_menu( 'secondary') ): ?>
+		<button class="menu-trigger  menu--open  js-menu-trigger">
+		<?php get_template_part( 'assets/svg/menu-bars-svg' ); ?>
+		</button>
+		<nav id="site-navigation" class="menu-wrapper" role="navigation">
+			<button class="menu-trigger  menu--close  js-menu-trigger">
+
+				<?php get_template_part( 'assets/svg/close-icon-svg' ); ?>
+
+			</button>
+
+			<?php
+			wp_nav_menu( array(
+				'container' => false,
+				'theme_location' => 'primary',
+				'menu_class' => 'primary-menu',
+				'fallback_cb' => false,
+				'walker' => new Listable_Walker_Nav_Menu(),
+			) );
+			wp_nav_menu( array(
+				'container_class' => 'secondary-menu-wrapper',
+				'theme_location' => 'secondary',
+				'menu_class' => 'primary-menu secondary-menu',
+				'fallback_cb' => false,
+				'walker' => new Listable_Walker_Nav_Menu(),
+			) ); ?>
+
+		</nav>
+		<?php endif; ?>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
